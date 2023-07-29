d6ca4abcf9437171b580d464b81c9cc3043b85b9
Nishat Tasnim <117283757+nishattasnim3646@users.noreply.github.com>
2023-07-29 21:00:17 +0600

    Add files via upload

---
+++ pattern4/4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n, row,col;
+    printf("Enter row number:");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++){
+        for(col=n-row;col>=1;col--){
+            printf(" ");
+        }
+        for(col=1;col<=row;col++){
+            printf("%d",row%2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
